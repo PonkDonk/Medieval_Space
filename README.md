2c66b6b54b76b551dd1c56f11b99ddcc40e6c046
PonkDonk <adamminns29@gmail.com>
2026-02-22 15:13:47 +0000

    Generating Unreal Project Medieval_SpaceCreating Unreal 5 Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Medieval_Space
+
+Developed with Unreal Engine 5
---
+++ Source/Medieval_Space/Medieval_Space.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Medieval_Space.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Medieval_Space, "Medieval_Space" );
